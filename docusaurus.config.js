11df6b29f37d1709fe660948316169ad05e92846
Davirain <davirain.yin@gmail.com>
2022-08-19 22:53:50 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,15 +16,15 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'davirain', // Usually your GitHub org/user name.
+  projectName: 'blog', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans','en'],
   },
 
   presets: [
@@ -34,17 +34,17 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/DaviRain-Su/my-blog/tree/main',
+          routeBasePath: '/',
+          path: 'docs/',
+          exclude: ['docs/**', '**/_*.{md,mdx}'],
+          editCurrentVersion: false,
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/DaviRain-Su/my-blog/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -56,11 +56,16 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      metadata: [
+        { name: 'viewport', content: 'width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no' },
+        { name: 'keyword', content: 'web,js,css,ts,rust,wiki' },
+      ],
       navbar: {
         title: 'Davirian Site',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
+          style: { borderRadius: '50%' },
         },
         items: [
           {
@@ -71,8 +76,8 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/DaviRain-Su',
-            label: 'GitHub',
+            href: 'https://github.com/DaviRain-Su/my-blog',
+            label: 'Davirain github',
             position: 'right',
           },
         ],
